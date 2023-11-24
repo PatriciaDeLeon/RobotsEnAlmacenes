78ef5eb809fe1ea31c728eb07dfbf209eaf4392a
eugenio-turcott <A01198189@tec.mx>
2023-11-23 22:04:09 -0600

    Realicé modificaciones en el model y server sobre la implementación de API e instancias de cargadores y cajas

--- RobotsEnAlmacenes/server.py
+++ RobotsEnAlmacenes/server.py
@@ -1,8 +1,19 @@
 import mesa
-
-from .model import Habitacion, Robot, Celda, Estante, Cargador, Caja
+from flask import Flask, jsonify, request
+from threading import Thread
+from mesa.visualization.ModularVisualization import ModularServer
+from mesa.visualization.modules import CanvasGrid
+from model import Habitacion, Robot, Celda, Estante, Cargador, Caja
 
 MAX_NUMBER_ROBOTS = 20
+MAX_CARGADORES = 3
+MAX_CAJAS = 100
+MAX_STEPS = 3600
+robots = 2
+chargers = 3
+in_boxes = 6
+out_boxes = 3
+steps = 120
 
 
 def agent_portrayal(agent):
@@ -49,12 +60,60 @@ model_params = {
         1, # Salto del slider
         description = "Escoge cuántos robots deseas implementar en el modelo",
     ),
+    "num_cargadores": mesa.visualization.Slider(
+        "Número de Cargadores",
+        2, # Valor inicial al correr la simulación
+        1, # Valor mínimo
+        MAX_CARGADORES, # Valor máximo
+        1, # Salto del slider
+        description = "Escoge cuántos cargadores deseas implementar en el modelo",
+    ),
+    "num_cajas_entrada": mesa.visualization.Slider(
+        "Número de Cajas de Entrada",
+        6, # Valor inicial al correr la simulación
+        1, # Valor mínimo
+        MAX_CAJAS, # Valor máximo
+        1, # Salto del slider
+        description = "Escoge cuántas cajas de entrada deseas implementar en el modelo",
+    ),
+    "num_cajas_salida": mesa.visualization.Slider(
+        "Número de Cajas de Salida",
+        3, # Valor inicial al correr la simulación
+        1, # Valor mínimo
+        MAX_CAJAS, # Valor máximo
+        1, # Salto del slider
+        description = "Escoge cuántas cajas de salida deseas implementar en el modelo",
+    ),
+    "num_steps": mesa.visualization.Slider(
+        "Número de Steps",
+        120, # Valor inicial al correr la simulación
+        60, # Valor mínimo
+        MAX_STEPS, # Valor máximo
+        60, # Salto del slider
+        description = "Escoge cuántos steps deseas implementar en el modelo",
+    ),
    
     "M": 20,
     "N": 20,
 }
 
+app = Flask(__name__)
+
+def make_model(num_agentes, num_cargadores, num_cajas_entrada, num_cajas_salida, num_steps, M, N):
+    return Habitacion(
+        num_agentes=num_agentes,
+        num_cargadores=num_cargadores,
+        num_cajas_entrada=num_cajas_entrada,
+        num_cajas_salida=num_cajas_salida,
+        num_steps=num_steps,
+        M=M,
+        N=N
+    )
+
 server = mesa.visualization.ModularServer(
-    Habitacion, [grid],
+    make_model, [grid],
     "botCleaner", model_params, 8521
 )
+
+if __name__ == "__main__":
+    app.run(port=5000)
